24b33768a178b8ab05c63cb8dd40686b957f517f
gaurav.negi <mail.gauravsinghnegi@gmail.com>
2016-09-11 00:34:33 +0530

    adding datetime format to file

--- time_python.py
+++ time_python.py
@@ -1,8 +1,12 @@
 __author__ = 'goldi'
 import time
+import datetime
 tick=time.time()
 print tick
 localtime = time.localtime(time.time())
 print "Local current time :", localtime
-
+print('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
+print('Timestamp: {:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()))
+print('Date now: %s' % datetime.datetime.now())
+print('Date today: %s' % datetime.date.today())
 
