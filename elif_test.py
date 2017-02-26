2cb543cf213ae8ab95d4a09be2bc4806412b3c7b
gaurav1987singh <mail.gauravsinghnegi@gmail.com>
2017-02-26 22:57:53 +0530

    updating new message

--- elif_test.py
+++ elif_test.py
@@ -4,8 +4,8 @@ age=int(raw_input('Enter age:'))
 if name == 'Alice':
  print('Hi, Alice.')
 elif age < 12:
- print('You are not Alice, kiddo.')
+ print('You are not Alice, kiddo!.')
 elif age > 2000:
- print('Unlike you, Alice is not an undead, immortal vampire.')
+ print('Unlike you!, Alice is not an undead, immortal vampire.')
 elif age > 100:
- print('You are not Alice, grannie.')
+ print('You are not Alice, grannie.!')
