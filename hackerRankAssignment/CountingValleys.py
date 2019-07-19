9770eaf80f8f3d93de5b2f8c71332ade52e8cec3
gaurav negi <mail.gauravsinghnegi@gmail.com>
2019-07-19 20:19:54 +0530

    committing Counting Valley solution

--- hackerRankAssignment/CountingValleys.py
+++ hackerRankAssignment/CountingValleys.py
@@ -1,1 +1,18 @@
 # __name__ = 'goldi'
+def countingValleys(n, steps):
+    print(n,steps)
+    level, numOfValley = 0, 0
+    for step in steps:
+        if step == 'U':
+            level += 1
+            if level == 0:
+                numOfValley += 1
+        elif step == 'D':
+            level -= 1
+    return numOfValley
+
+n= int(input("Enter number of steps"))
+steps= input ("Enter path travelled")
+
+result = countingValleys(n,steps)
+print('result',result)
